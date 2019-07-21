9c0e4ef59b3cef502c71f1ddd72b980769f3c163
Angry Nimfa <angrynimfa@Angrys-iMac.local>
2019-07-21 10:55:39 -0700

    range comb

--- level4/range_comb/range_comb.c
+++ level4/range_comb/range_comb.c
@@ -27,24 +27,13 @@ void put_arr(int *arr, int n)
 	printf("\n");
 }
 
-// void	swap(int *arr, int i, int j)
-// {
-// 	int c;
-
-// 	c = arr[i];
-// 	arr[i] = arr[j];
-// 	arr[j] = c;
-// 	//put_arr(arr, 2);
-// 	//return (arr);
-// }
-
-void	swap(int *a, int *b)
+void	swap(int *arr, int i, int j)
 {
 	int c;
 
-	c = *a;
-	*a = *b;
-	*b = c;
+	c = arr[i];
+	arr[i] = arr[j];
+	arr[j] = c;
 }
 
 int		ft_fact(int nbr)
@@ -65,13 +54,44 @@ int		ft_fact(int nbr)
 	return (f);
 }
 
-// void	permitation(int *num, int **arr, int size)
-// {
-// 	int i;
+int	*permitation(int *num, int n)
+{
+	int i;
+	int j;
+	int right;
+	int left;
 
-	
-// }
+	i = n - 2;
+	while (i >= 0 && num[i] > num[i + 1])
+		i--;
+	if (i < 0)
+		return (0);
+	j = n - 1;
+	while (num[i] > num[j])
+		j--;
+	swap(num, i, j);
+	left = i + 1;
+	right = n - 1;
+	while (left < right)
+	{
+		swap(num, left, right);
+		left++;
+		right--;
+	}
+	return (num);
+}
 
+void copy_arr(int *dst, int *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+}
 
 int    **range_comb(int n)
 {
@@ -87,26 +107,37 @@ int    **range_comb(int n)
 	while (i < n)
 	{
 		num[i] = i;
-		arr[i] = (int *)malloc(sizeof(int) * n);
 		i++;
 	}
-	//permitation(num, arr, fact);
-	// i = 0;
-	// while (i < fact)
-	// {
-	// 	put_arr(arr[i], n);
-	// 	i++;
-	// }
+	arr[0] = (int *)malloc(sizeof(int) * n);
+	copy_arr(arr[0], num, n);
+	i = 1;
+	while (i < fact)
+	{
+		arr[i] = (int *)malloc(sizeof(int) * n);
+		copy_arr(arr[i], arr[i - 1], n);
+		permitation(arr[i], n);
+		i++;
+	}
+	free(num);
+	arr[fact] = NULL;
 	return (arr);
 }
 
 int main(void)
 {
 	int n;
+	int i;
+	int **arr;
 
+	i = 0;
 	n = 3;
-	range_comb(n);
-	//printf("%d\n", ft_fact(n));
+	arr = range_comb(n);
+	while (arr[i])
+	{
+		put_arr(arr[i], n);
+		i++;
+	}
 	return 0;
 }
 
